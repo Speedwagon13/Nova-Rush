8125181d9fcf720773ee83d7b0478c89ac34558c
Speedwagon13 <kperras3@gatech.edu>
2017-10-19 18:19:05 -0400

    Changes Pre oct 19th meeting

--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -7,14 +7,6 @@ public class cameraMovement : MonoBehaviour {
     public GameObject playerShip;
     private Rigidbody body;
 
-    private void Start()
-    {
-        
-        body = gameObject.GetComponent<Rigidbody>();
-
-        transform.position = playerShip.transform.position + new Vector3(0, 19, -11);
-    }
-
     void Update () {
         if (playerShip != null)
         {
